2b1e66710272b71508f23e29e93d9a00034567c6
Alec K <boltakziy@gmail.com>
2025-04-13 14:25:19 +0900

    4000

---
+++ 4000Series/4000/design_4000IC.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+typedef  int  INPUT;
+typedef  int  OUTPUT;
+
+OUTPUT do_NOR(INPUT I1, INPUT I2, INPUT I3);
+OUTPUT do_NOT(INPUT I);
+
+int main()
+{
+	int idx;
+
+	for (idx = 0; idx < 2; ++idx) {
+		printf("NOR 4000 %d\n\n", idx + 1);
+		printf("1%c : 0\t2%c : 0\t3%c : 0\tO%c : %d\n", idx + 65, idx + 65, idx + 65, idx + 65, do_NOR(0, 0, 0));
+		printf("1%c : 0\t2%c : 0\t3%c : 1\tO%c : %d\n", idx + 65, idx + 65, idx + 65, idx + 65, do_NOR(0, 0, 1));
+		printf("1%c : 0\t2%c : 1\t3%c : 0\tO%c : %d\n", idx + 65, idx + 65, idx + 65, idx + 65, do_NOR(0, 1, 0));
+		printf("1%c : 0\t2%c : 1\t3%c : 1\tO%c : %d\n", idx + 65, idx + 65, idx + 65, idx + 65, do_NOR(0, 1, 1));
+		printf("1%c : 1\t2%c : 0\t3%c : 0\tO%c : %d\n", idx + 65, idx + 65, idx + 65, idx + 65, do_NOR(1, 0, 0));
+		printf("1%c : 1\t2%c : 0\t3%c : 1\tO%c : %d\n", idx + 65, idx + 65, idx + 65, idx + 65, do_NOR(1, 0, 1));
+		printf("1%c : 1\t2%c : 1\t3%c : 0\tO%c : %d\n", idx + 65, idx + 65, idx + 65, idx + 65, do_NOR(1, 1, 0));
+		printf("1%c : 1\t2%c : 1\t3%c : 1\tO%c : %d\n\n\n", idx + 65, idx + 65, idx + 65, idx + 65, do_NOR(1, 1, 1));
+	}
+
+	printf("Inverter 4000 1\n\n");
+	printf("IC : 0\tOC : %d\n", do_NOT(0));
+	printf("IC : 1\tOC : %d\n\n\n", do_NOT(1));
+
+	return 0;
+}
+
+OUTPUT do_NOR(INPUT I1, INPUT I2, INPUT I3)
+{
+	OUTPUT O;
+
+	O = (~(I1 | I2 | I3)) & 0x0001;
+
+	return O;
+}
+
+OUTPUT do_NOT(INPUT I)
+{
+	OUTPUT O;
+
+	O = (~(I)) & 0x0001;
+
+	return O;
+}
