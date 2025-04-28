ee4c7b6133420f8909b08ce9f898007eadebc8c3
Alec K <boltakziy@gmail.com>
2025-04-28 22:41:33 +0900

    7414

---
+++ 7400Series/7414/design_7414IC.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef  int  INPUT;
+typedef  int  OUTPUT;
+
+OUTPUT do_NOT(INPUT I);
+
+int main()
+{
+	int idx;
+
+	for (idx = 0; idx < 6; ++idx) {
+		printf("Inverter 7414 %d\n\n", idx + 1);
+		printf("A%d : 0\tY%d : %d\n", idx + 1, idx + 1, do_NOT(0));
+		printf("A%d : 1\tY%d : %d\n\n\n", idx + 1, idx + 1, do_NOT(1));
+	}
+
+	return 0;
+}
+
+OUTPUT do_NOT(INPUT I)
+{
+	OUTPUT O;
+
+	O = (~I) & 0x0001;
+
+	return O;
+}
