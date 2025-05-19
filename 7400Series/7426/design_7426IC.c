f7349461cc03e385bf01bfeae5f73631148301d7
Alec K <boltakziy@gmail.com>
2025-05-20 07:22:11 +0900

    7426

---
+++ 7400Series/7426/design_7426IC.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef  int  INPUT;
+typedef  int  OUTPUT;
+
+OUTPUT do_NAND(INPUT I1, INPUT I2);
+
+int main()
+{
+	int idx;
+
+	for (idx = 0; idx < 4; ++idx) {
+		printf("NAND 7426 %d\n\n", idx + 1);
+		printf("A%d : 0\tB%d : 0\tY%d : %d\n", idx + 1, idx + 1, idx + 1, do_NAND(0, 0));
+		printf("A%d : 0\tB%d : 1\tY%d : %d\n", idx + 1, idx + 1, idx + 1, do_NAND(0, 1));
+		printf("A%d : 1\tB%d : 0\tY%d : %d\n", idx + 1, idx + 1, idx + 1, do_NAND(1, 0));
+		printf("A%d : 1\tB%d : 1\tY%d : %d\n\n\n", idx + 1, idx + 1, idx + 1, do_NAND(1, 1));
+	}
+
+	return 0;
+}
+
+OUTPUT do_NAND(INPUT I1, INPUT I2)
+{
+	OUTPUT O;
+
+	O = (~(I1 & I2)) & 0x0001;
+
+	return O;
+}
